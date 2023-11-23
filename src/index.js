fc6ea284c4386db1f49e78072f506e69f47a65e6
rmagur1203 <rmagur1203@naver.com>
2023-11-23 16:36:52 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import * as tags from './ToolBoxTagsComponents';
 export const Xml = tags.Xml;
 export const Block = tags.Block;
 export const Category = tags.Category;
+export const Field = tags.Field;
 
 export default BlocklyDrawer;
